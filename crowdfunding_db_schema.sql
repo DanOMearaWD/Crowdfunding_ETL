cffecf89f55a4aa08350366f50f3629f84a1c092
Dan O'Meara <danomearawd@gmail.com>
2024-11-11 15:02:35 -0500

    Create crowdfunding_db_schema.sql

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE IF NOT EXISTS "Category" (
+    "category_id" VARCHAR(5) PRIMARY KEY,
+    "category" VARCHAR(25) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "Subcategory" (
+    "subcategory_id" VARCHAR(8) PRIMARY KEY,
+    "subcategory" VARCHAR(25) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "Contacts" (
+    "contact_id" CHAR(4) PRIMARY KEY,
+    "first_name" VARCHAR(25) NOT NULL,
+    "last_name" VARCHAR(25) NOT NULL,
+    "email" VARCHAR(256) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "Campaign" (
+    "cf_id" VARCHAR(4) PRIMARY KEY,
+    "contact_id" CHAR(4) NOT NULL REFERENCES "Contacts" ("contact_id"),
+    "company_name" VARCHAR(50) NOT NULL,
+    "description" VARCHAR(100) NOT NULL,
+    "goal" MONEY NOT NULL,
+    "pledged" MONEY NOT NULL,
+    "outcome" VARCHAR(10) NOT NULL,
+    "backers_count" INT NOT NULL,
+    "country" CHAR(2) NOT NULL,
+    "currency" CHAR(3) NOT NULL,
+    "launch_date" DATE NOT NULL,
+    "end_date" DATE NOT NULL,
+    "category_id" VARCHAR(5) NOT NULL REFERENCES "Category" ("category_id"),
+    "subcategory_id" VARCHAR(8) NOT NULL REFERENCES "Subcategory" ("subcategory_id")
+);
